68008786561d3b25054c95255980d7bbccdd8277
Leon Chung <leon.karho.chung@gmail.com>
2023-02-28 15:10:22 -0500

    final product

--- Dog.js
+++ Dog.js
@@ -3,8 +3,28 @@ class Dog {
 		Object.assign(this, data);
 	}
 
-	test() {
-		console.log(`hi my name is ${this.name}`);
+	renderDog() {
+		const { age, avatar, bio, name } = this;
+		let html = `
+			<img class='dog-image' src="${avatar}" alt="${name}" />
+			<div class="dog-description">
+				<h1>${name}, ${age}</h1>
+				<h2>${bio}</h2>
+			</div>
+		`;
+
+		document.getElementById("dog-profile-content").innerHTML = html;
+	}
+
+	renderStamp(stampString) {
+		let html = `
+			<div 
+				class='${stampString === "nope" ? "stamp red-stamp" : "stamp"}' id='stamp'>
+				${stampString}
+			</div>`;
+		document
+			.getElementById("dog-profile-content")
+			.insertAdjacentHTML("afterbegin", html);
 	}
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-import { dogs } from "./data.js";
+import { dogs as dogsData } from "./data.js";
 import Dog from "./Dog.js";
 
-const dog1 = new Dog(dogs[1]);
+let currentDogIndex = 0;
+let currentDog = new Dog(dogsData[currentDogIndex]);
 
-dog1.test();
+currentDog.renderDog();
+
+function nextDog() {
+	currentDogIndex++;
+	if (!dogsData[currentDogIndex]) {
+		let html = `<div class='no-more'><p>No more dogs in your area</p></div>`;
+
+		document.getElementById("dog-profile-content").innerHTML = html;
+		document.getElementById("buttons-group").style.display = "none";
+
+		return;
+	}
+	currentDog = new Dog(dogsData[currentDogIndex]);
+	currentDog.renderDog();
+}
+
+document.getElementById("like-btn").addEventListener("click", () => {
+	currentDog.renderStamp("liked");
+	setTimeout(() => {
+		nextDog();
+	}, 1500);
+});
+
+document.getElementById("dislike-btn").addEventListener("click", () => {
+	currentDog.renderStamp("nope");
+	setTimeout(() => {
+		nextDog();
+	}, 1500);
+});
